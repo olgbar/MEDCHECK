3172988959d63150e2520a3fb13855f9ec118a6c
olgbar <barbaraolguin02@gmail.com>
2024-10-24 22:35:15 -0300

    btn Descarga- btn Alerta - Comentarios

---
+++ js/script.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function () {
+    document.getElementById("btnSubmit").addEventListener("click", function () {
+        alert("Gracias por contactarnos.");
+    });
+});
